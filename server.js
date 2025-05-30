8ff9dcfd46b317f8598b030140dfd7bea828bdec
Adam Noveskey <a_noveskey@yahoo.com>
2025-05-30 13:45:18 -0400

    task - implemented send-email endpoint and utilized environment variables

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express = require('express');
+require('dotenv').config();
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const express = require('express');
+const nodemailer = require('nodemailer');
+
 const app = express();
 const port = 3000;
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 let visitorCount = 0;
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "http://evbpmusic.com", "https://evbpmusic.com"];
 
 app.use(cors({
     origin: function (origin, callback) {
@@ -17,8 +23,46 @@ app.use(cors({
     }
   }));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('There is nothing to see here. Perhaps you meant to visit the frontend?');
+});
+
+app.post('/api/send-email', (req, res) => {
+  const { email, message, subject } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false, 
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD
+    },
+    tls: {
+      rejectUnauthorized: !isDevelopment // Disable TLS certificate validation in development
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    replyTo: email,
+    subject: subject,
+    text: message,
+    to: process.env.EMAIL_RECIPIENT
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      return res.status(500).json({ error: 'Failed to send email' });
+    }
+
+    console.log('Email sent:', info.response);
+    res.json({ message: 'Email sent successfully' });
+  });
 });
 
 app.get('/api/visitor-count', (req, res) => {

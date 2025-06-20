3a6549479b9417432148989ddc2b0dd4fd291bc1
Adam Noveskey <a_noveskey@yahoo.com>
2025-06-20 16:00:52 -0400

    task - added guestbook endpoints and unit tests

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
+require('isomorphic-fetch');
+
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 const cors = require('cors');
+const ErrorCodes = require('./error_codes.js');
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const { ClientSecretCredential } = require('@azure/identity');
 const { Client } = require('@microsoft/microsoft-graph-client');
-require('isomorphic-fetch');
 
 const app = express();
 const port = 3000;
@@ -13,7 +18,7 @@ const isDevelopment = process.env.NODE_ENV === 'development';
 
 let visitorCount = 0;
 
-const allowedOrigins = ["http://localhost:5173", "http://evbpmusic.com", "https://evbpmusic.com"];
+const allowedOrigins = [process.env.ALLOWED_ORIGIN];
 
 app.use(cors({
     origin: function (origin, callback) {
@@ -32,6 +37,29 @@ app.get('/', (req, res) => {
   res.send('There is nothing to see here. Perhaps you meant to visit the frontend?');
 });
 
+app.get('/api/guestbook-entries', (req, res) => {
+  const filePath = path.join(__dirname, 'guestbook_entries.json');
+  let entries = [];
+
+  if (fs.existsSync(filePath)) {
+    const rawData = fs.readFileSync(filePath, 'utf8');
+    if (rawData.trim().length > 0) {
+      entries = JSON.parse(rawData);
+    }
+  }
+
+  if (entries.length === 0) {
+    return res.status(404).json({ code: ErrorCodes.DATA_UNAVAILABLE, message: 'No guestbook entries found.' });
+  } else {
+    res.status(200).json(entries);
+  }
+});
+
+app.get('/api/visitor-count', (req, res) => {
+    visitorCount += 1;
+    res.json({ count: visitorCount });
+})
+
 app.post('/api/send-email', async (req, res) => {
   const { email, message, subject } = req.body;
 
@@ -46,7 +74,7 @@ app.post('/api/send-email', async (req, res) => {
       process.env.OAUTH_CLIENT_SECRET
     );
 
-    const tokenResponse = await credential.getToken("https://graph.microsoft.com/.default");
+    const tokenResponse = await credential.getToken(process.env.GRAPH_TOKEN_URL);
 
     const graphClient = Client.init({
       authProvider: (done) => {
@@ -86,10 +114,105 @@ app.post('/api/send-email', async (req, res) => {
   }
 });
 
-app.get('/api/visitor-count', (req, res) => {
-    visitorCount += 1;
-    res.json({ count: visitorCount });
-})
+app.post('/api/send-validation-code-to-email', async (req, res) => {
+  const hashCode = crypto.randomBytes(Math.ceil(42 / 2)).toString('hex').slice(0, 42);
+  const filePath = path.join(__dirname, 'guestbook_users.json');
+  const { email, username } = req.body;
+  let userData = {};
+  
+  if (!email || !username) {
+    return res.status(400).json({ error: 'Missing required fields.' });
+  }
+
+  if (fs.existsSync(filePath)) {
+    const rawData = fs.readFileSync(filePath, 'utf8');
+    if (rawData.trim().length > 0) {
+      userData = JSON.parse(rawData);
+    }
+  }
+
+  const userAlreadyExists = Object.values(userData).some(user => user.email === email || user.username === username);
+
+  if (userAlreadyExists) {
+    return res.status(400).json({ code: ErrorCodes.USER_ALREADY_EXISTS, message: 'Everybody gets one. If you feel you have reached this message in error, please contact us - include your email and username - and we\'ll see what we can do to help!' });
+  } else {
+    userData[hashCode] = { username, email };
+    fs.writeFileSync(filePath, JSON.stringify(userData, null, 2));
+  }
+  
+  // send email with validation code
+  const messageContent = `Hello ${username},\n\nYour validation code is: ${hashCode}\n\nPlease copy and paste it into the validation code field on the guestbook page to continue signing the guestbook!`;
+  const messageSubject = 'Validation Code for ' + username;
+  const credential = new ClientSecretCredential(
+      process.env.OAUTH_TENANT_ID,
+      process.env.OAUTH_CLIENT_ID,
+      process.env.OAUTH_CLIENT_SECRET
+  );
+  const tokenResponse = await credential.getToken(process.env.GRAPH_TOKEN_URL);
+  const graphClient = Client.init({
+    authProvider: (done) => {
+      done(null, tokenResponse.token);
+    }
+  });
+
+  await graphClient.api('/users/' + process.env.EMAIL_SENDER + '/sendMail')
+  .post({
+    message: {
+      body: {
+        contentType: 'Text',
+        content: messageContent
+      },
+      subject: messageSubject,
+      toRecipients: [
+        {
+          emailAddress: {
+            address: email
+          }
+        }
+      ],
+    }
+  })
+
+  res.json({ message: 'A validation code has been sent to the email address you provided. Please enter it into the validation code input field to continue.' });
+});
+
+app.post('/api/sign-guestbook', async (req, res) => {
+  const { username, message } = req.body;
+  const filePath = path.join(__dirname, 'guestbook_entries.json');
+  let guestbookEntries = [];
+  if (fs.existsSync(filePath)) {
+    const rawData = fs.readFileSync(filePath, 'utf8');
+    if (rawData.trim().length > 0) {
+      guestbookEntries = JSON.parse(rawData);
+    }
+  }
+
+  guestbookEntries.push({ username, message });
+
+  fs.writeFileSync(filePath, JSON.stringify(guestbookEntries, null, 2));
+
+  res.status(201).json({ message: 'Thanks for signing my guestbook. You rock!' });
+});
+
+app.post('/api/validate-user', async (req, res) => {
+  const { validationCode } = req.body;
+
+  const filePath = path.join(__dirname, 'guestbook_users.json');
+  let userData = {};
+
+  if (fs.existsSync(filePath)) {
+    const rawData = fs.readFileSync(filePath, 'utf8');
+    if (rawData.trim().length > 0) {
+      userData = JSON.parse(rawData);
+    }
+  }
+
+  if (userData[validationCode]) {
+    res.status(200).json({ message: 'User validation successful. You can now sign the guestbook!', user: userData[validationCode] });
+  } else {
+    res.status(401).json({ code: ErrorCodes.USER_ENTRY_NOT_FOUND, message: 'User entry not found. Please contact the site admin.' });
+  }
+});
 
 if (require.main === module) {
   app.listen(port, () => {
--- test/server.test.js
+++ test/server.test.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const fs = require('fs');
 const sinon = require('sinon');
 const request = require('supertest');
 const proxyquire = require('proxyquire');
@@ -17,12 +18,18 @@ const graphClientStub = {
         })
     })
 };
+const fsStub = {
+    existsSync: sinon.stub().returns(true),
+    readFileSync: sinon.stub(),
+    writeFileSync: sinon.stub()
+};
 
 const consoleErrorStub = sinon.stub(console, 'error');
 
 const app = proxyquire('../server', {
     '@azure/identity': { ClientSecretCredential: credentialStub },
-    '@microsoft/microsoft-graph-client': { Client: graphClientStub }
+    '@microsoft/microsoft-graph-client': { Client: graphClientStub },
+    'fs': fsStub,
 });
 
 describe('Server', () => {
@@ -41,7 +48,43 @@ describe('Server', () => {
         expect(res.text).to.equal('There is nothing to see here. Perhaps you meant to visit the frontend?');
     });
 
-    it('should return a fail message on POST /api/send-email when no data is sent', async () => {
+    it('should have all routes registered', () => {
+            const routes = app._router.stack
+            .filter(r => r.route)
+            .map(r => r.route.path);
+
+            expect(routes).to.include('/');
+            expect(routes).to.include('/api/guestbook-entries');
+            expect(routes).to.include('/api/visitor-count');
+            expect(routes).to.include('/api/send-email');
+            expect(routes).to.include('/api/send-validation-code-to-email');
+            expect(routes).to.include('/api/sign-guestbook');
+            expect(routes).to.include('/api/validate-user');
+        });
+
+    describe('GET /api/guestbook-entries', () => {
+        it('should return guestbook entries on GET /api/guestbook-entries', async () => {
+            const mockEntries = [{ username: 'Test_User2', message: 'First!'}, { username: 'Test_User', message: 'Hello World' }];
+            fsStub.existsSync.returns(true);
+            fsStub.readFileSync.callsFake(() => JSON.stringify(mockEntries));
+
+            const res = await request(app).get('/api/guestbook-entries');
+            expect(res.status).to.equal(200);
+            expect(res.body).to.be.an('array');
+        });
+
+        it('should return 404 status on GET /api/guestbook-entries when guestbook entries file is empty', async () => {
+            fsStub.existsSync.returns(true);
+            fsStub.readFileSync.callsFake(() => JSON.stringify([]));
+
+            const res = await request(app).get('/api/guestbook-entries');
+            expect(res.status).to.equal(404);
+            expect(res.body).to.be.an('object');
+        });
+    });
+
+    describe('POST /api/send-email', () =>{
+        it('should return a fail message on POST /api/send-email when no data is sent', async () => {
         const res = await request(app).post('/api/send-email').send({});
         expect(res.status).to.equal(400);
         expect(res.body).to.have.property('error', 'Missing required fields.');
@@ -56,10 +99,112 @@ describe('Server', () => {
         expect(res.status).to.equal(500);
         expect(res.body).to.have.property('error', 'Failed to send email');
     });
+    });
 
-    it('should return a visitor count on GET /api/visitor-count', async () => {
+    describe('POST /api/send-validation-code-to-email', () => {
+        it('should return 400 error on POST /api/send-validation-code-to-email when no data is sent', async () => {
+            const res = await request(app).post('/api/send-validation-code-to-email').send({});
+            expect(res.status).to.equal(400);
+            expect(res.body).to.have.property('error', 'Missing required fields.');
+        });
+
+        it("should return a 400 error on POST /api/send-validation-code-to-email when email or username already exists", async () => {
+            fsStub.existsSync.returns(true);
+            fsStub.readFileSync.callsFake(() => JSON.stringify(
+                { 'mockhash12345': { email: 'johndoe@mocksite.com', username: 'user1' }}
+            ));
+
+            const res = await request(app).post('/api/send-validation-code-to-email').send({
+                email: 'johndoe@mocksite.com',
+                username: 'user2' // validating that the check will happen on either or both fields
+            });
+
+            expect(res.status).to.equal(400);
+            expect(res.body).to.have.property('code', 'USER_ALREADY_EXISTS');
+            expect(res.body).to.have.property('message', 'Everybody gets one. If you feel you have reached this message in error, please contact us - include your email and username - and we\'ll see what we can do to help!');
+        });
+
+        it('should write a new user to the users file on POST /api/send-validation-code-to-email', async () => {
+            fsStub.existsSync.returns(true);
+            fsStub.readFileSync.callsFake(() => JSON.stringify({}));
+            fsStub.writeFileSync.resetHistory();
+
+            const res = await request(app).post('/api/send-validation-code-to-email').send({
+                email: 'johndoe@mocksite.com',
+                username: 'user1'
+            });
+            expect(res.status).to.equal(200);
+            expect(fsStub.writeFileSync.calledOnce).to.be.true;
+            const writtenData = JSON.parse(fsStub.writeFileSync.getCall(0).args[1]);
+        });
+
+        it('should send an email with the validation code on POST /api/send-validation-code-to-email', async () => {
+            fsStub.existsSync.returns(true);
+            fsStub.readFileSync.callsFake(() => JSON.stringify({}));
+            fsStub.writeFileSync.resetHistory();
+            postStub.resetHistory();
+
+            const res = await request(app).post('/api/send-validation-code-to-email').send({
+                email: 'johndoe@mocksite.com',
+                username: 'user1'
+            });
+            expect(res.status).to.equal(200);
+            expect(postStub.calledOnce).to.be.true;
+            const emailArgs = postStub.getCall(0).args[0];
+            expect(emailArgs).to.have.property('message');
+            expect(emailArgs.message).to.have.property('body');
+            expect(emailArgs.message.body.contentType).to.equal('Text');
+            expect(emailArgs.message.body.content).to.include('Your validation code is:');
+            expect(res.body.message).to.equal('A validation code has been sent to the email address you provided. Please enter it into the validation code input field to continue.');
+        });
+    });
+
+    describe('POST /api/sign-guestbook', () => {
+        it('should write a new entry to the guestbook on POST /api/sign-guestbook', async () => {
+            fsStub.existsSync.returns(true);
+            fsStub.readFileSync.callsFake(() => JSON.stringify([]));
+            fsStub.writeFileSync.resetHistory();
+
+            const res = await request(app).post('/api/sign-guestbook').send({
+                username: 'Test_User',
+                message: 'Hello World',
+                validationCode: 'valid-code'
+            });
+            expect(res.status).to.equal(201);
+            expect(fsStub.writeFileSync.calledOnce).to.be.true;
+            const writtenData = JSON.parse(fsStub.writeFileSync.getCall(0).args[1]);
+            expect(writtenData).to.be.an('array');
+            expect(writtenData[0]).to.have.property('username', 'Test_User');
+            expect(writtenData[0]).to.have.property('message', 'Hello World');
+        });
+    });
+
+    describe('POST /api/validate-user', () => {
+        it('should return 401 error on POST /api/validate-user when user entry is not found', async () => {
+            fsStub.existsSync.returns(true);
+            fsStub.readFileSync.callsFake(() => JSON.stringify({}));
+
+            const res = await request(app).post('/api/validate-user').send({
+                validationCode: 'nonexistent-code'
+            });
+            expect(res.status).to.equal(401);
+            expect(res.body).to.have.property('code', 'USER_ENTRY_NOT_FOUND');
+        });
+
+        it('should return 200 status on POST /api/validate-user with valid validation code', async () => {
+            fsStub.existsSync.returns(true);
+            fsStub.readFileSync.callsFake(() => JSON.stringify({
+                'valid-code': { username: 'Test_User', email: 'johndoe@mocksite.com' }
+            }));
+
+        });
+    });
+
+    describe('GET /api/visitor-count', () => {
+        it('should return a visitor count on GET /api/visitor-count', async () => {
         const res = await request(app).get('/api/visitor-count');
         expect(res.status).to.equal(200);
         expect(res.body).to.have.property('count');
+    });
     });
 });
